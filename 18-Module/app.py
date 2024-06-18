b93738e87d16c805ddad4e4ba91db2bb31997fcb
Mel/ <tchoikuemel06@gmail.com>
2024-06-18 22:55:31 +0100

    module in python

---
+++ 18-Module/app.py
@@ -0,0 +1,10 @@
+import convertor
+from convertor import kg_to_lbs
+from utils import find_max
+
+numbers = [50, 60, 30, 59]
+max_number = find_max(numbers)
+print(max_number)
+
+print(kg_to_lbs(100) )
+print(convertor.kg_to_lbs(70))
