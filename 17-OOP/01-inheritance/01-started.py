2a3a9e2baefd717325ae85dfb37a9067c8695654
Mel/ <tchoikuemel06@gmail.com>
2024-06-18 22:55:00 +0100

    inheritance in python

---
+++ 17-OOP/01-inheritance/01-started.py
@@ -0,0 +1,23 @@
+class Mammal:
+  def walk(self):
+    print('Walk')
+
+class Dog(Mammal):
+  def bark(self):
+    print('bark')
+  
+  
+class Cat(Mammal):
+  def Meow(self):
+    print('Meow')
+
+# dog1 = Dog()
+
+# dog1.walk()
+# dog1.bark()
+
+cat1 = Cat()
+cat1.Meow(
+)
+
+cat1.walk()
